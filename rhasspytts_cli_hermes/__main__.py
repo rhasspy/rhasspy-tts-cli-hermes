4cd7f31893301a757b9618ccda43cd262537f414
Michael Hansen <hansen.mike@gmail.com>
2020-03-22 14:14:44 -0400

    Using hermes CLI utils

--- rhasspytts_cli_hermes/__main__.py
+++ rhasspytts_cli_hermes/__main__.py
@@ -4,6 +4,7 @@ import asyncio
 import logging
 
 import paho.mqtt.client as mqtt
+import rhasspyhermes.cli as hermes_cli
 
 from . import TtsHermesMqtt
 
@@ -27,32 +28,11 @@ def main():
     parser.add_argument(
         "--voices-command", help="Command to list voices (one per line)"
     )
-    parser.add_argument(
-        "--host", default="localhost", help="MQTT host (default: localhost)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=1883, help="MQTT port (default: 1883)"
-    )
-    parser.add_argument(
-        "--siteId",
-        action="append",
-        help="Hermes siteId(s) to listen for (default: all)",
-    )
-    parser.add_argument(
-        "--debug", action="store_true", help="Print DEBUG messages to the console"
-    )
-    parser.add_argument(
-        "--log-format",
-        default="[%(levelname)s:%(asctime)s] %(name)s: %(message)s",
-        help="Python logger format",
-    )
+
+    hermes_cli.add_hermes_args(parser)
     args = parser.parse_args()
 
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG, format=args.log_format)
-    else:
-        logging.basicConfig(level=logging.INFO, format=args.log_format)
-
+    hermes_cli.setup_logging(args)
     _LOGGER.debug(args)
 
     try:
@@ -70,7 +50,7 @@ def main():
         )
 
         _LOGGER.debug("Connecting to %s:%s", args.host, args.port)
-        client.connect(args.host, args.port)
+        hermes_cli.connect(client, args)
         client.loop_start()
 
         # Run event loop
